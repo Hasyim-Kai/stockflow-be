1c61f5b4a7a3f9dcf3ecb9c1e1034f9dc52b8417
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2024-05-08 11:14:11 +0700

    Refactor : add validation class-validator globally in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // for accessing .env globally
   const configService = app.get(ConfigService);
+  // for enabling class-validator globally
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, }),);
+
   await app.listen(configService.get('PORT') || 3179);
 }
 bootstrap();
