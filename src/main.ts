ae7d72ab3635062c3b7ea40ab4bfd7264df19071
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2024-05-10 14:56:32 +0700

    Feat : Add CORS & add prefix api url

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // for adding prefix after base url
+  app.setGlobalPrefix('api/v1');
   // for accessing .env globally
   const configService = app.get(ConfigService);
   // for enabling class-validator globally
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, }),);
+  // for enabling CORS
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
 
   await app.listen(configService.get('PORT') || 3179);
 }
