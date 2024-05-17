6b7cc1dd65bd9da99410c2f0a0996b0817e91d95
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2024-05-17 16:38:42 +0700

    Refactor : Edit catch error in onDelete master data

--- src/domain/master-data/product/product.service.ts
+++ src/domain/master-data/product/product.service.ts
@@ -92,6 +92,15 @@ export class ProductService {
     try {
       await this.prisma.product.delete({ where: { id } });
     } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') { // Unique constraint violation
+          throw new HttpException('Code already exists', HttpStatus.BAD_REQUEST);
+        } else if (error.code === 'P2003') {
+          throw new HttpException('Action cannot be done because it is related to other data', HttpStatus.BAD_REQUEST);
+        } else {
+          throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+      }
       throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
